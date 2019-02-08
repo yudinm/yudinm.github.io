3b66e1fb2ada53aa98d1e774af12d440de96629d
Michael Yudin <michael.s.yudin@gmail.com>
2019-02-08 18:19:24 +0300

    + added build todos redux firebase

---
+++ reactLessons/todoFirebase/react-redux-firebase/build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactLessons/todoFirebase/react-redux-firebase/build/precache-manifest.d7aa34549ec47ac81c63948fd4844f4e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactLessons/todoFirebase/react-redux-firebase/build/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
--- reactLessons/todoFirebase/react-redux-firebase/src/config/prod.js
+++ reactLessons/todoFirebase/react-redux-firebase/src/config/prod.js
@@ -1,1 +1,10 @@
-console.log("PROD!!!");
+export const FirebaseConfig = {
+    apiKey: "AIzaSyAu0MnWvRJuGSw3KCV3FTR8BuXITU_eaNw",
+    authDomain: "reduxedy.firebaseapp.com",
+    databaseURL: "https://reduxedy.firebaseio.com",
+    projectId: "reduxedy",
+    storageBucket: "reduxedy.appspot.com",
+    messagingSenderId: "281546425218"
+  };
+
+  
