92abf880a2dced1b2d526cefae34a44b569c0650
Michael Yudin <michael.s.yudin@gmail.com>
2019-02-11 14:00:18 +0300

    * bump"

---
+++ reactLessons/todoFirebase/react-redux-firebase/build/precache-manifest.091a8129298bb55fe8d66fa4b2773a88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af7b3790b5ba171923de",
+    "url": "/static/js/main.af7b3790.chunk.js"
+  },
+  {
+    "revision": "6dd4a885cdf6ccee44c9",
+    "url": "/static/js/1.6dd4a885.chunk.js"
+  },
+  {
+    "revision": "af7b3790b5ba171923de",
+    "url": "/static/css/main.a1f55072.chunk.css"
+  },
+  {
+    "revision": "c02cde184d3a06b3ae65695961ecad53",
+    "url": "/index.html"
+  }
+];
--- reactLessons/todoFirebase/react-redux-firebase/build/service-worker.js
+++ reactLessons/todoFirebase/react-redux-firebase/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactLessons/todoFirebase/react-redux-firebase/build/precache-manifest.d7aa34549ec47ac81c63948fd4844f4e.js"
+  "/precache-manifest.091a8129298bb55fe8d66fa4b2773a88.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactLessons/todoFirebase/react-redux-firebase/build/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
